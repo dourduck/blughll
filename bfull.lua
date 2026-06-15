a59614b9af6ada41721df76fa2454e1bb7d08b5b
dourduck <dourduck@gmail.com>
2026-06-15 16:08:31 -0600

    added cr.c to build script

--- bfull.lua
+++ bfull.lua
@@ -1,4 +1,7 @@
 #!/usr/bin/env lua
+os.execute("echo '[***BULIDING CR LIB***]'")
+os.execute("gcc cr.c -c -I./include/ -L./lib/ -lraylib -Wl,-rpath,$(pwd)/lib/ -lm -lpthread -ldl -lrt -lX11")
+
 os.execute("echo '[***BULIDING GAME LIB***]'")
 os.execute("gcc game.c -c -I./include/ -L./lib/ -lraylib -Wl,-rpath,$(pwd)/lib/ -lm -lpthread -ldl -lrt -lX11")
 
@@ -10,4 +13,4 @@ os.execute("gcc -c main.c -I./include/ -L./lib/ -lraylib -Wl,-rpath,$(pwd)/lib/ 
 
 os.execute("echo '[***LINKING***]'")
 os.execute(
-	"gcc -o game main.o game.o render.o -I./include/ -L./lib/ -lraylib -Wl,-rpath,$(pwd)/lib/ -lm -lpthread -ldl -lrt -lX11")
+	"gcc -o game main.o cr.o game.o render.o -I./include/ -L./lib/ -lraylib -Wl,-rpath,$(pwd)/lib/ -lm -lpthread -ldl -lrt -lX11")
